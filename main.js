18d0cae5e8052f8aef5a027939b5b423c2e992c5
Erym974 <remyrichard974@live.fr>
2023-06-19 14:35:04 +0200

    Exo9 Finished

--- main.js
+++ main.js
@@ -1,37 +1,83 @@
-function App(props) {
-    React.useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(usersObject => setUsers(usersObject))
-    }, [])
+const inputContext = React.createContext(false)
 
-    const [users, setUsers] = React.useState([]);
-
-
-
-    return(
-        <React.Fragment>
-        {
-            users.map((u) =>(
-                <UserCard key={u.id} user={u}/>
-            ))
-        }
-        </React.Fragment>
-    )
-}
-
-function UserCard({user}) {
-    
+const InputProvider = ({ children }) => {
+    const [formData, setFormData] = React.useState({ input: '', textarea: '', select: '' })
 
     return (
-        <ul className="user-card">
-            <li>{user.name}</li>
-            <li>{user.email}</li>
-            <li>{user.company.name}</li>
-            <li>{user.phone}</li>
-            <li>{user.website}</li>
-        </ul>
+        <inputContext.Provider value={{ formData, setFormData }}>
+            { children }
+        </inputContext.Provider>
     )
 }
+
+const InputText = () => {
+    const { formData, setFormData } = React.useContext(inputContext)
+
+    return (
+        <div>
+            <input type='text' placeholder='Enter your name' value={formData.input} onChange={({ target: { value } }) => { setFormData({ ...formData, input: value}) }}  />
+        </div>
+    )
+
+}
+
+const TextArea = () => {
+    const { formData, setFormData } = React.useContext(inputContext)
+
+    return (
+        <div>
+            <textarea onChange={({ target: { value } }) => { setFormData({ ...formData, textarea: value}) }} defaultValue={formData.textarea} >
+            </textarea>
+        </div>
+    )
+}
+
+const Select = () => {
+
+    const [options, setOptions] = React.useState(['toto', 'tata', 'tonton'])
+    const { formData, setFormData } = React.useContext(inputContext)
+
+    return (
+        <div>
+            <select onChange={({ target: { value } }) => { setFormData({ ...formData, select: value}) }} defaultValue={formData.select} >
+                <option value=''>Select an option</option>
+                {options.map((option, index) => {
+                    return (
+                        <option key={index} value={option}>{option}</option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
+
+const Form = () => {
+
+    const { formData } = React.useContext(inputContext)
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault(); 
+
+        alert(`Le formulaire a été soumis : \n\n  Input : ${formData.input} \n\n TextArea : ${formData.textarea} \n\n Select : ${formData.select}`)
+    }
+
+    return (
+        <form action=''>
+            <InputText />
+            <TextArea />
+            <Select />
+            <button type='submit' onClick={(evt) => { handleSubmit(evt) }} >Valider le formulaire</button>
+        </form>
+    )
+}
+
+const App = () => {
+    return(
+        <InputProvider>
+            <Form />
+        </InputProvider>
+    )
+}
+
 
 ReactDOM.render(<App/>, document.querySelector('#app'))
