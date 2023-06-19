196173a4e4eaddb278b558d4682689893ff37bf1
Erym974 <remyrichard974@live.fr>
2023-06-19 14:46:45 +0200

    Exo10 Finished

--- main.js
+++ main.js
@@ -1,288 +1,54 @@
-
-
-// -------------------------------------------
-// Étape 1 => On modifie le composant input
-// -------------------------------------------
-
-/* class NameForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('Le nom a été soumis : ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Nom :
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
-      );
-    }
-  }
- */
-
-
-function NameForm(props) {
-    const [name, setName] = React.useState('');
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        setName(e.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(`Le nom a été soumis: ${name}`);
-    }
-
-
-
-    return(
-    <form onSubmit={handleSubmit}>
-        <label>
-        Nom :
-        <input type="text" value={name} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Envoyer" />
-    </form>
-);
+const TYPE = {
+    BINARY: 'BINARY',
+    DECIMAL: 'DECIMAL'
 }
 
-
-
-// ReactDOM.render(<NameForm />, document.querySelector('#app'));
-
-
-
-
-
-
-// -------------------------------------------
-// Étape 2 => On modifie le composant textarea
-// -------------------------------------------
-
-
-
-/* class EssayForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: 'Écrivez un essai à propos de votre élément du DOM préféré'
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+function BaseNumberInput({ onChangeBase, placeholder, value, handleFocus }) {
+    const handleChange = e => {
+      placeholder === TYPE.BINARY ? onChangeBase( e.target.value, 10 ) : onChangeBase( e.target.value, 2 )
     }
   
-    handleChange(event) {
-      this.setState({value: event.target.value});
+    const handleChangeFocus = e => {
+      handleFocus(placeholder)
     }
-  
-    handleSubmit(event) {
-      alert('Un essai a été envoyé : ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Essay:
-            <textarea value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
-      );
-    }
-  } */
-
-function EssayForm(props) {
-    const [text, setText] = React.useState('Écrivez un essai à propos de votre élément du DOM préféré');
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        setText(e.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(`Le texte a été soumis: ${text}`);
-    }
-      
-    return(
-        <form onSubmit={handleSubmit}>
-          <label>
-            Essay:
-            <textarea rows={5} cols={35} value={text} onChange={handleChange} />
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
-
-      )
-
-
-  }
-
-
-
-//   ReactDOM.render(<EssayForm />, document.querySelector('#app'));
-
-
-
-
-
-// -------------------------------------------
-// Étape 3 => On modifie le composant select
-// -------------------------------------------
-
-/* class FlavorForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: 'coconut'};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('Votre parfum favori est : ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Choisissez votre parfum favori :
-            <select value={this.state.value} onChange={this.handleChange}>
-              <option value="grapefruit">Pamplemousse</option>
-              <option value="lime">Citron vert</option>
-              <option value="coconut">Noix de coco</option>
-              <option value="mango">Mangue</option>
-            </select>
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
-      );
-    }
-  } */
-
-function FlavorForm(props) {
-    const [value, setValue] = React.useState('coconut');
-    const handleChange = (e) => {
-        e.preventDefault();
-        setValue(e.target.value)
-
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(`la valeur sélectionnée est: ${value}`);
-    }
-
-    return(
-        <form onSubmit={handleSubmit}>
-          <label>
-            Choisissez votre parfum favori :
-            <select value={value} onChange={handleChange}>
-              <option value="grapefruit">Pamplemousse</option>
-              <option value="lime">Citron vert</option>
-              <option value="coconut">Noix de coco</option>
-              <option value="mango">Mangue</option>
-            </select>
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
-    );
-}
-
-
-
-//ReactDOM.render(<FlavorForm/>, document.querySelector('#app'));
-
-
-
-
-function MultiForm(props) {
-    const [inputs, setInputs] = React.useState({
-        name: '',
-        text: '',
-        value: "coconut"
-    });
-
     
-
-/*     
-
-    // useCallback n'est pas forcément le meilleur choix ici
-
-    
-    const handleChange = React.useCallback(
-        ({target: {name, value}}) => {
-            setInputs(state => ({...state, [name]: value}), []);
-            console.log(inputs);
-        },
-        [inputs]
+    return (
+      <input type="text" onFocus={handleChangeFocus} placeholder={placeholder} onChange={handleChange} value={value}/>
     )
+}
 
- */
 
+function App() {
     
-    const handleChange = ({target: {name, value}}) => {
-        setInputs(state => ({...state, [name]: value}), []);
+    const [binaryNumber, setBinaryNumber] = React.useState('')
+    const [decimalNumber, setDecimalNumber] = React.useState('')
+    const [focus, setFocus] = React.useState(TYPE.DECIMAL)
+  
+    const handleFocus = (value) => {
+      setFocus(value)
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        for(const name in inputs) {
-            console.log(`${name}: ${inputs[name]}`)
+  
+    const handleChange = (number, base) => {
+      base === 2 ? setBinaryNumber(_.toNumber( number ).toString(2)) : setDecimalNumber(parseInt(_.toNumber( number ), 2))
+    }
+  
+    return (
+      <div className="wrapper">
+        {focus === TYPE.BINARY &&
+          <React.Fragment>
+            <BaseNumberInput handleFocus={handleFocus} onChangeBase={handleChange} placeholder={TYPE.DECIMAL} value={decimalNumber} />
+            <BaseNumberInput handleFocus={handleFocus} onChangeBase={handleChange} placeholder={TYPE.BINARY} />
+          </React.Fragment>
         }
-    }
-
-
-    return(
-        <form onSubmit={handleSubmit}>
-            <label>
-                Nom :
-                <input type="text" name="name" value={inputs.name} onChange={handleChange} />
-            </label>
-
-            <label>
-                Essay:
-                <textarea name="text" value={inputs.text} onChange={handleChange} />
-            </label>
-
-            <label>
-                Choisissez votre parfum favori :
-                <select name="value" value={inputs.value} onChange={handleChange}>
-                    <option value="grapefruit">Pamplemousse</option>
-                    <option value="lime">Citron vert</option>
-                    <option value="coconut">Noix de coco</option>
-                    <option value="mango">Mangue</option>
-                </select>
-            </label>
-
-            <input type="submit" value="Envoyer" />
-        </form>
+        {focus === TYPE.DECIMAL &&
+          <React.Fragment>
+            <BaseNumberInput handleFocus={handleFocus} onChangeBase={handleChange} placeholder={TYPE.DECIMAL} />
+            <BaseNumberInput handleFocus={handleFocus} onChangeBase={handleChange} placeholder={TYPE.BINARY} value={binaryNumber} />
+          </React.Fragment>
+        }      
+      </div>
     )
-}
+  }
 
-ReactDOM.render(<MultiForm />, document.querySelector('#app'));
+
+ReactDOM.render(<App />, document.querySelector('#app'));
