5584ed1d814033e1b127e8da192f44efc3d5386f
Erym974 <remyrichard974@live.fr>
2023-06-19 12:22:09 +0200

    Exo7 Finished

--- main.js
+++ main.js
@@ -1,63 +1,53 @@
-function Clock(props) {
-    React.useEffect(() => {
-        tick();
-    }, []);
+const LoginContext = React.createContext(false);
+const LoginProvider = ({ children }) => {
+
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false)
+    const [user, setUser] = React.useState(null)
     
-    const [date, setDate] = React.useState(new Date());
-    const [textColor, setTextColor] = React.useState("black");
-    const intervalRef = React.useRef();
-    const tick = () => {
-        intervalRef.current = setInterval(() => {
-            setDate(new Date());
-        }, 1000)
-        
-    }
-
-    const changeColor = () => {
-        var randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`; 
-        setTextColor(randomColor);
-    }
-
-    const resetColor = () => {
-        setTextColor('black')
-    }
-
-    const stopClock = (e) => {
-        e.preventDefault();
-        clearInterval(intervalRef.current);
-    }
-
-
-    const restartClock = (e) => {
-        e.preventDefault();
-        tick();
-    }
-    
-
-/*     return (
-        <div>
-            <h1>Hello world</h1>
-            <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
-            <button onClick={changeColor}>Changer couleur</button>
-            <button onClick={resetColor}>Réinitialiser couleur</button>
-        </div>
-        ); */
-
-        // Solution Bonus
-
-        return (
-            <div>
-                <h1>Hello world</h1>
-                <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
-                <button onClick={stopClock}>Stop</button>
-                <button onClick={restartClock}>Reprendre</button>
-            </div>
-            );
+    return (
+        <LoginContext.Provider value={{ isLoggedIn, setIsLoggedIn, user, setUser }}>
+            {children}
+        </LoginContext.Provider>
+    )
 }
 
-ReactDOM.render(<Clock />, document.querySelector('#app'));
+const UserGreeting = () => {
+    const { setIsLoggedIn, user, setUser } = React.useContext(LoginContext)
+    return(
+        <div>
+            <h1>Bienvenue {user} !</h1>
+            <button onClick={() => { setIsLoggedIn(false); setUser(null) }}>Se déconnecter</button>
+        </div>
+    )
+}
+  
+const GuestGreeting = () => {
+    const { setIsLoggedIn, setUser } = React.useContext(LoginContext)
+    return (
+        <div>
+            <h1>Veuillez vous inscrire.</h1>
+            <input type="text" onChange={(e) => { setUser(e.target.value) }} />
+            <button onClick={() => { setIsLoggedIn(true) }}>S'inscrire</button>
+        </div>
+    )
+}
 
+const Greeting = () => {
+    const { isLoggedIn } = React.useContext(LoginContext)
+    return (
+        <div>
+            {(isLoggedIn) ? <UserGreeting /> : <GuestGreeting />}
+        </div>
+    )
+}
 
+const App = () => {
+    return (
+        <LoginProvider>
+            <Greeting />
+        </ LoginProvider>
+    )
 
+}
 
-
+ReactDOM.render(<App />, document.querySelector('#app'))
