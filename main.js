cab4021d161f1502b5bc47eb7e1373157f7ca9c5
Erym974 <remyrichard974@live.fr>
2023-06-19 11:52:49 +0200

    Exo6 Finished

--- main.js
+++ main.js
@@ -1,101 +1,30 @@
-/* 
-// Étape 1
-class Clock extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Bonjour, monde !</h1>
-          <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
-    }
-  }
-
-ReactDOM.render(<Clock date={new Date()}/>, document.querySelector('#app')); 
-
-*/
-
-
-/* 
-
-// Étape 2
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-  render() {
-    return (
-      <div>
-        <h1>Bonjour, monde !</h1>
-        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app')); 
-
-
- */
-
-
-
-// Étape 3
-
-/* class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(),1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        })
-    }
-
-    render() {
-        return (
-        <div>
-            <h1>Bonjour, monde !</h1>
-            <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-        );
-    }
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app'));  */
-
-
 function Clock(props) {
-    React.useEffect(() => {
-        tick();        
-    }, []);
     
     const [date, setDate] = React.useState(new Date());
-
-    const tick = () => {
-        setInterval(() => {
-            setDate(new Date())
-        }, 1000)
-    }
+    const timerActive = React.useRef(true);
+    const [color, setColor] = React.useState('#00000');
     
+    setInterval(() => {
+        if(timerActive.current === false) return
+        setDate(new Date())
+    }, 1000)
+    
+    const colories = () => setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`)
+    const reset = () => setColor('#000000')
+    const startAndStop = () => timerActive.current = !timerActive.current
 
     return (
-        <div>
+        <div>   
             <h1>Hello world</h1>
-            <h2>Il est {date.toLocaleTimeString()}.</h2>
+            <h2>Il est <span style={{ color: `${color}` }}>{date.toLocaleTimeString()}</span>.</h2>
+            <div>
+                <button onClick={colories}>Couleur aléatoire</button>
+                <button onClick={reset}>Reset couleur</button>
+            </div>
+            <p>{timerActive.current}</p>
+            <button onClick={startAndStop}>Pause/Reprendre</button>
         </div>
-        );
+    );
 }
 
 ReactDOM.render(<Clock />, document.querySelector('#app'));
